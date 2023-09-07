43f524fc3304411da861b4af91231643b554c17a
Sandhyamahi <123637827+Sandhyamahi@users.noreply.github.com>
2023-09-07 16:20:28 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,18 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+    function localStorage.setItem()
+{
+  player1_name="Arjun";
+}
+    //Set "player2_name" using localStorage.setItem() function
+    function localStorage.setItem()
+    {
+      player2_name="Rohan";
+    }
+    window.location = "game_page.html";
+}
+
